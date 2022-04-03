4fb32b9f761dd5fdc84c2123adbfaaba0ece2eb3
TheoDeb <theo.debefve@gmail.com>
2022-04-03 17:09:27 +0200

    :lipstick: Add New portfolio Window file

---
+++ WindowConection/NewWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowConection
+{
+    /// <summary>
+    /// Interaction logic for NewWindow.xaml
+    /// </summary>
+    public partial class NewWindow : Window
+    {
+        private bool CanClose { get; set; }
+        public NewWindow()
+        {
+            InitializeComponent();
+            CanClose = true;
+        }
+        public void CloseWindow()
+        {
+            CanClose = false;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = CanClose;
+            base.OnClosing(e);
+            Hide();
+        }
+    }
+}
