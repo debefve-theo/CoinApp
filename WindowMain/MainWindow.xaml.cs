6faf3b9cec18d979176a7f15d6e890ca1a3d1946
TheoDeb <theo.debefve@gmail.com>
2022-04-07 18:23:53 +0200

    :lipstick: Update Main Window file

--- WindowMain/MainWindow.xaml.cs
+++ WindowMain/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowSettings;
 
 namespace WindowMain
 {
@@ -20,9 +21,54 @@ namespace WindowMain
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly WindowSettings.MainWindow WindowSettings = new();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnMenuHome_Click(object sender, RoutedEventArgs e)
+        {
+            ColHome.Width = new GridLength(1, GridUnitType.Star);
+            ColTransaction.Width = new GridLength(0);
+            ColRepartition.Width = new GridLength(0);
+            ColWatchlist.Width = new GridLength(0);
+        }
+
+        private void BtnMenuTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            ColHome.Width = new GridLength(0);
+            ColTransaction.Width = new GridLength(1, GridUnitType.Star);
+            ColRepartition.Width = new GridLength(0);
+            ColWatchlist.Width = new GridLength(0);
+        }
+
+        private void BtnMenuRepartition_Click(object sender, RoutedEventArgs e)
+        {
+            ColHome.Width = new GridLength(0);
+            ColTransaction.Width = new GridLength(0);
+            ColRepartition.Width = new GridLength(1, GridUnitType.Star);
+            ColWatchlist.Width = new GridLength(0);
+        }
+
+        private void BtnMenuWatchlist_Click(object sender, RoutedEventArgs e)
+        {
+            ColHome.Width = new GridLength(0);
+            ColTransaction.Width = new GridLength(0);
+            ColRepartition.Width = new GridLength(0);
+            ColWatchlist.Width = new GridLength(1, GridUnitType.Star);
+        }
+
+        private void BtnMenuParametre_Click(object sender, RoutedEventArgs e)
+        {
+            WindowSettings.Show();
+            WindowSettings.Focus();
+            WindowSettings.Activate();
+        }
+
+        private void BtnMenuDeconexion_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
