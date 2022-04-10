61b26c65dc84964f56ee6f8d068f235ec9874f2f
TheoDeb <theo.debefve@gmail.com>
2022-04-10 16:50:24 +0200

    :rocket: Deploy Library Crypto

---
+++ LibraryCrypto/ClassAPI.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace LibraryCrypto
+{
+    public class ClassAPI
+    {
+        private static string API_KEY = "4be2bcce-2f54-488c-8ff5-8e9b4c96dea8";
+        private static int NBR_CRYPTO = 200; // 200 -> 1 API credits
+
+        private static List<Crypto> mainCryptoList = new List<Crypto>();
+
+        public static List<Crypto> GetCryptoList()
+        {
+            try
+            {
+                dynamic dobj = MakeAPICAll();
+
+                try
+                {
+                    for (int i = 0; i < NBR_CRYPTO; i++)
+                    {
+                        Crypto CryptoPlus = new Crypto(
+                            (int)dobj["data"][i]["id"],
+                            (string)dobj["data"][i]["name"],
+                            (string)dobj["data"][i]["symbol"],
+                            (double)dobj["data"][i]["quote"]["EUR"]["price"],
+                            (double)dobj["data"][i]["quote"]["EUR"]["volume_24h"],
+                            (double)dobj["data"][i]["quote"]["EUR"]["percent_change_1h"],
+                            (double)dobj["data"][i]["quote"]["EUR"]["percent_change_24h"],
+                            (double)dobj["data"][i]["quote"]["EUR"]["percent_change_7d"],
+                            (double)dobj["data"][i]["quote"]["EUR"]["market_cap"]
+                        );
+
+                        mainCryptoList.Add(CryptoPlus);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Méthode GetCryptoList : Erreur lors de l'ajout a la liste");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Méthode GetCryptoList : Erreur lors de la connection a l'API");
+            }
+
+            return mainCryptoList;
+        }
+
+        static dynamic MakeAPICAll()
+        {
+            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = "1";
+            queryString["limit"] = NBR_CRYPTO.ToString();
+            queryString["convert"] = "EUR";
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("Accepts", "application/json");
+            string dataString = client.DownloadString(URL.ToString());
+
+
+            dynamic dobj = JsonConvert.DeserializeObject<dynamic>(dataString);
+
+            return dobj;
+        }
+    }
+}
---
+++ LibraryCrypto/ClassCrypto.cs
@@ -0,0 +1,121 @@
+﻿namespace LibraryCrypto
+{
+    public class Crypto
+    {
+        #region VARIABLES
+        private int _id;
+        private string? _name;
+        private string? _symbol;
+        private double _price;
+        private double _volume24h;
+        private double _change1h;
+        private double _change1d;
+        private double _change1w;
+        private double _marketcap;
+        #endregion
+
+        #region CONSTRUCTEURS
+        public Crypto()
+        {
+            _id = 0;
+            _name = null;
+            _symbol = null;
+            _price = 0;
+            _volume24h = 0;
+            _change1h = 0;
+            _change1d = 0;
+            _change1w = 0;
+            _marketcap = 0;
+        }
+        public Crypto(int id, string name, string symbol, double price, double volume24h, double change1h, double change1d, double change1w, double marketcap)
+        {
+            _id = id;
+            _name = name;
+            _symbol = symbol;
+            _price = price;
+            _volume24h = volume24h;
+            _change1h = change1h;
+            _change1d = change1d;
+            _change1w = change1w;
+            _marketcap = marketcap;
+        }
+        public Crypto(Crypto c)
+        {
+            _id = c.Id;
+            _name = c.Name;
+            _symbol = c.Symbol;
+            _price = c.Price;
+            _volume24h = c._volume24h;
+            _change1h = c.Change1h;
+            _change1d = c.Change1d;
+            _change1w = c.Change1w;
+            _marketcap = c.Marketcap;
+        }
+        #endregion
+
+        #region GET/SET
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Symbol
+        {
+            get { return _symbol; }
+            set { _symbol = value; }
+        }
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public double Volume24h
+        {
+            get { return _volume24h; }
+            set { _volume24h = value; }
+        }
+        public double Change1h
+        {
+            get { return _change1h; }
+            set { _change1h = value; }
+        }
+        public double Change1d
+        {
+            get { return _change1d; }
+            set { _change1d = value; }
+        }
+        public double Change1w
+        {
+            get { return _change1w; }
+            set { _change1w = value; }
+        }
+        public double Marketcap
+        {
+            get { return _marketcap; }
+            set { _marketcap = value; }
+        }
+        #endregion
+
+        #region METHODES
+        public void Affiche()
+        {
+            Console.WriteLine("Crypto: " + ToString());
+        }
+        public override string ToString()
+        {
+            return "\n\tId :\t" + Id + "\n\tName :\t" + Name + "\n\tSymbol :\t" + Symbol + "\n\tPrice :\t" + Price + "\n\tVolume 24h :\t" + Volume24h + "\n\t% 1 hour :\t" + Change1h + "\n\t% 1 day :\t" + Change1d + "\n\t% 1 week :\t" + Change1w + "\n\tMarket cap :\t" + Marketcap + "\n";
+        }
+        #endregion
+
+        public static int Hello()
+        {
+            int i = 4;
+            return i;
+        }
+    }
+}
