d232b8c01ae133113017b235887e9e5f32b91f9a
Theo Debefve <theo.debefve@gmail.com>
2022-04-27 17:37:46 +0200

    :bricks: Infrastructure changes

---
+++ ClassLibrary/ClassAPI.cs
@@ -0,0 +1,75 @@
+﻿// Copyright © 2022 - Theo Debefve
+
+using System;
+using System.Net;
+using System.Web;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using Model;
+using ViewModel;
+
+namespace ClassLibrary
+{
+    public static class ClassApi
+    {
+        private const string ApiKey = "4be2bcce-2f54-488c-8ff5-8e9b4c96dea8";
+        private const int NbrCrypto = 200; // 200 -> 1 API credits
+
+        public static CryptoViewModel? CurrentCrypto;
+
+        public static ObservableCollection<CryptoViewModel> Items;
+
+
+        public static ObservableCollection<CryptoViewModel> GetCryptoList()
+        {
+            var dobj = MakeApiCAll();
+
+            string codeErreur = dobj["status"]["error_code"];
+            string messageErreur = dobj["status"]["error_message"];
+
+            for (var i = 0; i < NbrCrypto; i++)
+            {
+                CurrentCrypto = new CryptoViewModel(new Crypto()
+                {
+                    Id = dobj["data"][i]["id"],
+                    Name = dobj["data"][i]["name"],
+                    Symbol = dobj["data"][i]["symbol"],
+                    Price = dobj["data"][i]["quote"]["EUR"]["price"],
+                    Volume24H = dobj["data"][i]["quote"]["EUR"]["volume_24h"],
+                    Change1H = dobj["data"][i]["quote"]["EUR"]["percent_change_1h"],
+                    Change1D = dobj["data"][i]["quote"]["EUR"]["percent_change_24h"],
+                    Change1W = dobj["data"][i]["quote"]["EUR"]["percent_change_7d"],
+                    Marketcap = dobj["data"][i]["quote"]["EUR"]["market_cap"]
+                });
+
+                Items.Add(CurrentCrypto);
+            }
+
+            return Items;
+        }
+
+        private static dynamic MakeApiCAll()
+        {
+            var url = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = "1";
+            queryString["limit"] = NbrCrypto.ToString();
+            queryString["convert"] = "EUR";
+
+            url.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", ApiKey);
+            client.Headers.Add("Accepts", "application/json");
+            string dataString = client.DownloadString(url.ToString());
+
+
+            var dobj = JsonConvert.DeserializeObject<dynamic>(dataString);
+
+            return dobj;
+        }
+    }
+}
---
+++ ClassLibrary/ClassCalcul.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ClassCalcul
+    {
+        #region METHODES TOTAL
+
+        public static float CalculSoldeActuel()
+        {
+            float result = 0;
+
+            // prendre chaque crypto possédée et calculer avec la valeur actuelle
+            // peut etre une boucle avec la méthode CalculValeurTotalCrypto
+
+            return result;
+        }
+
+        public static float CalculSolde24H()
+        {
+            float result = 0;
+
+            // prendre chaque crypto possédée et calculer avec la valeur 24h
+
+            return result;
+        }
+
+        public static float CalculPourcentage24H()
+        {
+            float result = ((CalculSoldeActuel() / CalculSolde24H() * 100) - 100);
+            return result;
+        }
+
+        public static float CalculValeurTotalAchat()
+        {
+            float result = 0;
+
+            // récupérer pour chaque transaction quantité * prix par token et les additionner
+
+            return result;
+        }
+
+        public static float CalculGainPerteTotal()
+        {
+            float result = CalculSoldeActuel() - CalculValeurTotalAchat();
+            return result;
+        }
+
+        #endregion
+
+        #region METHODES CRYPTO
+
+        public static float CalculValeurTotalCrypto(int id)
+        {
+            float result = 0;
+
+            // prendre toute les transaction d'une crypto (id param) et recup quantity et la valeur actuel pour faire le total
+
+            return result;
+        }
+
+        public static float CalculGainPerteCrypto(int id)
+        {
+            float result = 0;
+
+            // prendre toute les transaction la méthode CalculPrixAvecFraisTransaction
+
+            return result;
+        }
+
+        public static float CalculAllocationCrypto(int id)
+        {
+            float result = CalculSoldeActuel() / CalculValeurTotalCrypto(id);
+            return result;
+        }
+
+        #endregion
+
+        #region METHODES TRANSACTION
+
+        public static float CalculPrixSansFraisTransaction(double id)
+        {
+            float result = 0;
+
+            // recup la transaction sa quantite et son prix par token
+
+            return result;
+        }
+
+        public static float CalculPrixAvecFraisTransaction(double id)
+        {
+            float result = 0;
+
+            // recup la transaction sa quantite et son prix par token et les frais
+
+            return result;
+        }
+
+        #endregion
+    }
+}
+
