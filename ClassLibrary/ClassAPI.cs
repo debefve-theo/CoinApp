894e726e6179e1a70f385724f5722c0e2e41cced
Theo Debefve <theo.debefve@gmail.com>
2022-05-04 10:38:53 +0200

    :rocket: Update Class Library

--- ClassLibrary/ClassAPI.cs
+++ ClassLibrary/ClassAPI.cs
@@ -1,56 +1,17 @@
 ﻿// Copyright © 2022 - Theo Debefve
 
-using System;
 using System.Net;
 using System.Web;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Runtime.CompilerServices;
 using Newtonsoft.Json;
-using Model;
-using ViewModel;
 
 namespace ClassLibrary
 {
     public static class ClassApi
     {
         private const string ApiKey = "4be2bcce-2f54-488c-8ff5-8e9b4c96dea8";
-        private const int NbrCrypto = 200; // 200 -> 1 API credits
+        public const int NbrCrypto = 200; // 200 -> 1 API credits
 
-        public static CryptoViewModel? CurrentCrypto;
-
-        public static ObservableCollection<CryptoViewModel> Items;
-
-
-        public static ObservableCollection<CryptoViewModel> GetCryptoList()
-        {
-            var dobj = MakeApiCAll();
-
-            string codeErreur = dobj["status"]["error_code"];
-            string messageErreur = dobj["status"]["error_message"];
-
-            for (var i = 0; i < NbrCrypto; i++)
-            {
-                CurrentCrypto = new CryptoViewModel(new Crypto()
-                {
-                    Id = dobj["data"][i]["id"],
-                    Name = dobj["data"][i]["name"],
-                    Symbol = dobj["data"][i]["symbol"],
-                    Price = dobj["data"][i]["quote"]["EUR"]["price"],
-                    Volume24H = dobj["data"][i]["quote"]["EUR"]["volume_24h"],
-                    Change1H = dobj["data"][i]["quote"]["EUR"]["percent_change_1h"],
-                    Change1D = dobj["data"][i]["quote"]["EUR"]["percent_change_24h"],
-                    Change1W = dobj["data"][i]["quote"]["EUR"]["percent_change_7d"],
-                    Marketcap = dobj["data"][i]["quote"]["EUR"]["market_cap"]
-                });
-
-                Items.Add(CurrentCrypto);
-            }
-
-            return Items;
-        }
-
-        private static dynamic MakeApiCAll()
+        public static dynamic MakeApiCAll()
         {
             var url = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
 
